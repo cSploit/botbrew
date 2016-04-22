78e91ea870e12340a36ab3339e6b3047a032d235
Eliott T <DeveloppSoft@users.noreply.github.com>
2016-04-22 17:35:35 +0200

    Changed build system

---
+++ make_botbrew.py
@@ -0,0 +1,22 @@
+# Make the goals separately
+# Just a simple python script to make pkg and print the logs if it fails
+
+from sys import argv
+from os import system
+
+fail, success = 0, 0
+
+goals = argv[1:]
+
+for goal in goals:
+	print "Making %s..." % goal
+	make = system("make -s %s > build.log" % goal)
+	if make != 0: # Fail ?
+		system("cat build.log") # Show the logs
+		fail += 1
+	else:
+		success += 1
+
+print "Failed: %s" % str(fail)
+print "Succesfull: %s" % str(success)
+print "Total: %s" % str(len(goals))
