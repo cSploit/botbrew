4f1726ceb9dcc4d917e46e578ff3917cc87c27f9
Eliott T <DeveloppSoft@users.noreply.github.com>
2016-04-24 14:17:51 +0200

    Add the ability to disabled packages

--- make_botbrew.py
+++ make_botbrew.py
@@ -5,19 +5,22 @@ from sys import argv
 from os import system
 
 fail, success = 0, 0
+disabled = [] # Put packages name you don't want to compile...
 
 goals = argv[1:]
 
 for goal in goals:
-	print "Making %s..." % goal
-	make = system("make %s 2> build.log" % goal)
-	if make != 0: # Fail ?
-		print "%s failed" % goal
-		system("cat build.log") # Show the logs
-		fail += 1
-	else:
-		success += 1
+	if goal not in disabled:
+		print "Making %s..." % goal
+		make = system("make %s 2> build.log" % goal)
+		if make != 0: # Fail ?
+			print "%s failed" % goal
+			system("cat build.log") # Show the logs
+			fail += 1
+		else:
+			success += 1
 
 print "Failed: %s" % str(fail)
 print "Succesfull: %s" % str(success)
+print "Disabled: %s" % str(len(disabled))
 print "Total: %s" % str(len(goals))
